16e966693dda4c34d4c59bfeaa5bc7485c012229
thanhdo3044 <phamthanhdo18@gmail.com>
2022-11-02 11:54:12 +0700

    calss

--- src/components/function/button/Button.js
+++ src/components/function/button/Button.js
@@ -1,21 +1,79 @@
-import './button.scss'
-function Button({children,primary = false, sizebtn = false}){
 
-    let classes =''
-    if(primary){
-        classes ='primary'
-    }else if(sizebtn){
-        classes='sizebtn'
+// function Button({children,primary = false, sizebtn = false}){
+
+
+
+//     let classes =''
+//     if(primary){
+//         classes ='primary'
+//     }else if(sizebtn){
+//         classes='sizebtn'
+//     }
+
+//     return(
+//         <button className={`button ${classes}`}>
+//             <span>
+//                 {children}
+//             </span>
+//         </button>
+//     )
+// }
+
+
+// export default Button;
+
+import React from "react";
+import './button.scss'
+
+
+class Button extends React.Component{
+    constructor(props){
+        super(props)
+        this.state={
+            showCounter: true,
+        };
+        this.onClickMe = this.onClickMe.bind(this)
     }
 
-    return(
-        <button className={`button ${classes}`}>
+    onClickMe(){
+        this.setState({
+            showCounter: false,
+        })
+    }
+    render(){
+        let temps = '';
+        if(this.props.primary){
+            temps ='primary'
+        }else if(this.props.sizebtn)(
+            temps = 'sizebtn'
+        )
+        return (
+            <>   
+            <button className={`button ${temps}`} onClick={this.onClickMe}>
             <span>
-                {children}
+                {this.props.children}
             </span>
-        </button>
-    )
+            </button>
+            {this.state.showCounter && <Show />}
+            </>
+        )
+    }
 }
 
+class Show extends React.Component{
+    render(){
+        return(
+            <h1>Show !</h1>
+        )
+    }
+    componentDidMount() {
+        console.log('did mount');
+    }
 
-export default Button;
+    componentWillUnmount() {
+        console.log('un mount');
+    }
+    
+}
+
+export default Button
